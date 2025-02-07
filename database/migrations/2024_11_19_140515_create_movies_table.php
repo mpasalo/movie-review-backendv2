ea08281f5467a0892c64dbf553e67b82ec573850
MarcPasalo <marcpasalo@gmail.com>
2025-02-08 05:47:45 +0800

    add field and add unique on title

--- database/migrations/2024_11_19_140515_create_movies_table.php
+++ database/migrations/2024_11_19_140515_create_movies_table.php
@@ -13,10 +13,11 @@ return new class extends Migration
     {
         Schema::create('movies', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('title')->unique();
             $table->string('image');
             $table->string('description');
             $table->string('director');
+            $table->year('year');
             $table->timestamps();
         });
     }
