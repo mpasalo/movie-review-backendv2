818b606a6f87d2f5ac19ac8d27820346cc195120
MarcPasalo <marcpasalo@gmail.com>
2025-02-08 05:49:46 +0800

    remove unused and fix fetching of movies to include reviews and users

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -11,17 +11,8 @@ class MovieController extends Controller
 {
     public function index()
     {
-        return Movie::with('review')->get();
-    }
-
-    public function filterByReview()
-    {
-        $movies = Movie::all();
-
-        $filteredMovies = $movies->filter(function ($value, $key) {
-            return $value->hasReview();
-        })->values();
-
-        return $filteredMovies;
+        return Movie::with(['reviews' => function ($query) {
+            $query->with(['user']);
+        }])->get();
     }
 }
